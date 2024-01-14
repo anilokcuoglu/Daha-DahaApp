18d0e5a3ebd7f0c5f1074a7ae2a5c7fe36d1c52f
Anil OKÇUOĞLU <anilokcuoglu@gmail.com>
2024-01-14 14:11:31 +0300

    deviceCheck commit

--- demoProject/src/pages/InitialPage/InitialPage.tsx
+++ demoProject/src/pages/InitialPage/InitialPage.tsx
@@ -4,28 +4,30 @@ import {useApiGetTagList} from '../../hooks/useApi';
 import styles from './InitialPage.styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {SvgIcons} from '../../components';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import TagList from '../../components/TagList/TagList';
 import CarouselCard from '../../components/Carousel/CarouselCard';
 
 const InitialPage = () => {
   return (
     <SafeAreaView edges={['top']} style={styles.container}>
-      <View style={styles.topBar}>
-        <SvgIcons.DahaDaha width={81} height={40} />
-        <View style={styles.rightTopBar}>
-          <TouchableOpacity style={styles.loginButton}>
-            <Text style={styles.loginText}>Giriş Yap</Text>
-          </TouchableOpacity>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.topBar}>
+          <SvgIcons.DahaDaha width={81} height={40} />
+          <View style={styles.rightTopBar}>
+            <TouchableOpacity style={styles.loginButton}>
+              <Text style={styles.loginText}>Giriş Yap</Text>
+            </TouchableOpacity>
 
-          <TouchableOpacity style={styles.userProfile}>
-            <SvgIcons.Profile width={16} height={17} />
-          </TouchableOpacity>
+            <TouchableOpacity style={styles.userProfile}>
+              <SvgIcons.Profile width={16} height={17} />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
 
-      <TagList />
-      <CarouselCard />
+        <TagList />
+        <CarouselCard />
+      </ScrollView>
     </SafeAreaView>
   );
 };
